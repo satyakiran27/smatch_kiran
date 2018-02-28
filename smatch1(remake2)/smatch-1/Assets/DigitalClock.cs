f9bda4ee3249269209496818d0da8b57a8b67b1d
Abhiram <subramanyam4572@gmail.com>
2018-03-01 00:32:45 +0530

    upload commit

---
+++ smatch1(remake2)/smatch-1/Assets/DigitalClock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DigitalClock : MonoBehaviour {
+    DateTime DateFetcher;
+    public GameObject Hours, Minutes, Seconds;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DateFetcher = DateTime.Now;
+        print("print: " + DateFetcher);
+        Hours.GetComponent<TextMesh>().text = DateFetcher.Hour.ToString();
+        Minutes.GetComponent<TextMesh>().text = DateFetcher.Minute.ToString();
+        Seconds.GetComponent<TextMesh>().text = DateFetcher.Second.ToString();
+
+    }
+}
---
+++ smatch1(remake2)/smatch-1/Assets/VRbutton.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class VRbutton: MonoBehaviour, IVirtualButtonEventHandler
+{ 
+   
+    private GameObject fun;
+    private GameObject call;
+    private GameObject clock;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < vbs.Length; ++i)
+        {
+            // Register with the virtual buttons TrackableBehaviour
+            vbs[i].RegisterEventHandler(this);
+            Debug.Log(vbs[i].name);
+        }
+
+
+        fun = transform.Find("Gangnamstyle").gameObject;
+        call = transform.Find("Ring").gameObject;
+        clock = transform.Find("Clock").gameObject;
+
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+       
+
+        switch (vb.VirtualButtonName)
+        {
+            case "Fun":
+                fun.SetActive(true);
+                clock.SetActive(false);
+                Debug.Log("BTN Pressed fun");
+                StartCoroutine(delay());
+                Debug.Log("Log the ID of my trackable: " + vb.name);
+                break;
+            case "Call":
+                call.SetActive(true);
+                clock.SetActive(false);
+                Debug.Log("Log the ID of my trackable: " + vb.name);
+                Debug.Log("BTN Pressed call");
+                StartCoroutine(delay_1());
+                break;
+           
+        }
+
+
+    }
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(12);
+        fun.SetActive(false);
+        clock.SetActive(true);
+    }
+    IEnumerator delay_1()
+    {
+        yield return new WaitForSeconds(5);
+        call.SetActive(false);
+        clock.SetActive(true);
+    }
+
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log(" BTN Realesed");
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+   
+
+}
